ade9bfeb5759aa79fe779c35d7f4688a556acd10
fahlirismunandar29fa <fahlirismunandar@gmail.com>
2021-04-06 17:44:15 +0700

    view landing page laravel

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\LandingController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/home', function () {
+//     return view('pages.landing-page.index');
+// });
+
+
+Route::resource('/', LandingController::class);
+Route::get('/clubs', [LandingController::class, 'clubs'])->name('landing.clubs');
+Route::get('/players', [LandingController::class, 'players'])->name('landing.players');
+Route::get('/managers', [LandingController::class, 'managers'])->name('landing.managers');
+Route::get('/stadium', [LandingController::class, 'stadium'])->name('landing.stadium');
